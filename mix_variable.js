09e3f32475e31f985da4b01cdc38b70d2276471c
kailash <kailashkumartkg@gmail.com>
2023-07-27 23:59:46 +0530

    initail commit

---
+++ mix_variable.js
@@ -0,0 +1,191 @@
+// var a=9
+// var b=7
+// console.log(a+b)
+
+// var a=90
+// var b=9
+// var a=6
+// var b=7
+// console.log(a+b)
+
+// var a=9
+// var b=6
+// var a=19
+// var b=11
+// console.log(a-b)
+
+// var a=90
+// var b=10
+// console.log(a-b)
+
+// var a=27
+// var b=3
+// console.log(a/b)
+
+// var b=90
+// var a=5
+// var b=80
+// var a=5
+// console.log(b/a)
+
+
+// var day="sunday"
+// if(day=="sunday"){
+//     console.log("rice dal")
+// }
+// else{
+//     console.log("panir matar")
+// }
+
+// var readline= require("readline-sync")
+// var day =readline.question("enter the any your day=============")
+// if(day=="sunday"){
+//     console.log("aalu chana")
+// }
+// else if(day=="monday"){
+//     console.log("rice dal")
+// }
+// else if(day=="tuesday"){
+//     console.log("chiken roti")
+// }
+// else if(day=="wenesday"){
+//     console.log("kari rice")
+// }
+// else if(day=="thuresday"){
+//     console.log("matan rice")
+// }
+// else if(day=="friday"){
+//     console.log("biryani")
+// }
+// else{
+//     console.log("paw baji")
+// }
+
+// var month="january"
+// if(month=="january"){
+//     console.log("mela")
+// }
+// else{
+//     console.log("pujaa")
+// }
+
+// var readline =require("readline-sync")
+// var month =readline.question("enter the any month===============")
+// if(month=="january"){
+//     console.log("mela")
+// }
+// else if(month=="feaburay"){
+//     console.log("pujaa")
+// }
+// else if(month=="march"){
+//     console.log("holi")
+// }
+// else if(month=="april"){
+//     console.log("ramnami")
+// }
+// else if(month=="may"){
+//     console.log("birthday")
+// }
+// else if(month=="june"){
+//     console.log("garmi")
+// }
+// else if(month=="julay"){
+//     console.log("barish")
+// }
+// else if(month=="august"){
+//     console.log("rakhi")
+// }
+// else if(month=="september"){
+//     console.log("dhup")
+// }
+// else if(month=="octumber"){
+//     console.log("diwali")
+// }
+// else if(month=="november"){
+//     console.log("chat pujaa")
+// }
+// else{
+//     console.log("nothing")
+// }
+
+// max_number======================
+
+// var readline =require("readline-sync")
+// var num1 =readline.questionInt("enter the any num1==================")
+// var num2 =readline.questionInt("enter the any num2=================")
+// var num3 =readline.questionInt("enter the any num3====================")
+// if(num1>num2 && num1>num3){
+//     console.log(num1,"num1 max number")
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(num2,"num2 max number")
+// }
+// else{
+//     console.log(num3,"num3 max number")
+// }
+
+
+// // nested_if_else=========================================
+// var readline =require("readline-sync")
+// var num1 =readline.questionInt("enter the any num1==================")
+// var num2 =readline.questionInt("enter the any num2=================")
+// var num3 =readline.questionInt("enter the any num3====================")
+// if(num1>num2){
+//     if(num1>num3){
+//         console.log(num1,"num1 num2 and num3 se bada hai")
+//     }
+//     else{
+//         console.log(num3,"num3 bada hai num1 and num2 se")
+//     }
+// }
+// else if(num2>num1){
+//     if(num2>num3){
+//         console.log(num2,"num2 bada hai num1 and num3 se")
+//     }
+//     else{
+//         console.log(num3,"num3 bada hai num2 and num1 se ")
+//     }
+// }
+// else{
+//     console.log(num3,"num3 bada hai num2 and num1 se")
+// }
+
+// even and odd============================================
+// var num=29
+// if(num%2==0){
+//     console.log(num,"even")
+// }
+// else{
+//     console.log(num,"odd")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num============")
+// if(num%2==0){
+//     console.log(num,"even")
+// }
+// else{
+//     console.log(num,"odd")
+// }
+
+
+// // posstive_and_neagtive===============================
+
+// var num=6
+// if(num>0){
+//     console.log(num,"posstive")
+// }
+// else{
+//     console.log(num,"neagtive")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter the any num===============")
+// if(num>0){
+//     console.log(num,"posstive")
+// }
+// else{
+//     console.log(num,"neagtive")
+// }
+
+    
---
+++ variable.js
@@ -0,0 +1,194 @@
+// var a = 29
+// if (a % 2 == 0) {
+//     console.log(a,"even")
+// }
+// else {
+//     console.log(a,"odd")
+// }
+
+// num1 = 90
+// if (num1 % 2 ==0) {
+//     console.log(num1,"even")
+// }
+// else {
+//     console.log(num1,"odd")
+// }
+
+// var readline = require("readline-sync")
+// var num1 = readline.questionInt("enter the any num1=================")
+// if (num1 % 2 == 0) {
+//     console.log(num1,"even")
+// }
+// else {
+//     console.log(num1,"odd")
+// }
+
+// posstive_and_neagtive==============================================
+// var num1 = -67
+// if (num1 > 0) {
+//     console.log(num1,"posstive")
+// }
+// else {
+//     console.log(num1,"neagtive")
+// }
+
+// var readline = require("readline-sync")
+// var num1 = readline.questionInt("enter the any num1===============")
+// if (num1 > 0) {
+//     console.log(num1,"posstive")
+// }
+// else {
+//     console.log(num1,"neagtive")
+// }
+
+// var day = "sunday";
+// if (day == "sunday") {
+//     console.log("panir matar")
+// }
+// else {
+//     console.log("rice dal")
+// }
+
+// var readline = require("readline-sync");
+// var day=readline.question("enter the any your day============")
+// if (day == "sunday") {
+//     console.log("panir matar")
+// }
+// else if (day == "monday") {
+//     console.log("rice dal")
+// }
+// else if (day == "tuesday") {
+//     console.log("roti sabli")
+// }
+// else if (day == "wenesday") {
+//     console.log("matar dal")
+// }
+// else if (day == "thuresday") {
+//     console.log("chiken rice")
+// }
+// else if (day == "friday") {
+//     console.log("matan rice")
+// }
+// else {
+//     console.log("puri sabji")
+// }
+
+// var month = "janueary";
+// if (month == "janueary") {
+//     console.log("mela")
+// }
+// else {
+//     console.log("pujaa")
+// }
+
+// var readline = require("readline-sync")
+// var month = readline.question("enter the any your month=============")
+// if (month == "janueary") {
+//     console.log("mela")
+// }
+// else if (month == "febuary") {
+//     console.log("puja")
+// }
+// else if (month == "march") {
+//     console.log("holi")
+// }
+// else if (month == "april") {
+//     console.log("ramnami")
+// }
+// else if (month == "may") {
+//     console.log("barithday")
+// }
+// else if (month == "june") {
+//     console.log("sadi")
+// }
+// else if (month == "july") {
+//     console.log("garmi")
+// }
+// else if (month == "august") {
+//     console.log("raki")
+// }
+// else if (month == "september") {
+//     console.log("techersday")
+// }
+// else if (month == "octumber") {
+//     console.log("diwali")
+// }
+// else if (month == "november") {
+//     console.log("chat puja")
+// }
+// else {
+//     console.log("thandi")
+// }
+
+// max===================================================
+
+// var readline = require("readline-sync")
+// var num1 = readline.questionInt("enter the any num1=============")
+// var num2 = readline.questionInt("enter the any num2=============")
+// var num3 = readline.questionInt("enter the any num3==============")
+// if (num1 > num2 && num1 > num3) {
+//     console.log(num1,"num1 max number hai")
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     console.log(num2,"num2 max number hai")
+// }
+// else {
+//     console.log(num3,"num3 max number hai")
+// }
+
+
+// nested if=========================================================
+
+// var readline = require("readline-sync")
+// var num1 = readline.questionInt("enter the any num1=============")
+// var num2 = readline.questionInt("enter the any num2=============")
+// var num3 = readline.questionInt("enter the any num3==============")
+// if (num1 > num2) {
+//     if (num1 > num2) {
+//         console.log(" num1 num2 and num3 se bada hai")
+//     }
+//     else {
+//         console.log(num3,"num3 bada hai")
+//     }
+// }
+// else if (num2 > num1) {
+//     if (num2 > num3) {
+//         console.log("num2 num1 and num3 se bada hai")
+//     }
+//     else {
+//         console.log( "num3 bada hai num2 se")
+//     }
+// }
+// else {
+//     console.log("num3 bada hai num1 and num2 se")
+// }
+
+// five_subject_parsentej========================================
+// var readline = require("readline-sync")
+// var math = readline.questionInt("enter the any num1=======================")
+// var hindi = readline.questionInt("enter the any num2=================");
+// var english = readline.questionInt("enter the any num3============");
+// var science = readline.questionInt("enter the any num4==============");
+// var chemistry = readline.questionInt("enter the any num5=====================");
+
+// var total = math + hindi + english + science + chemistry
+// var parsenteg = total / 5
+// if (parsenteg <90 && parsenteg >= 80) {
+//     console.log("A GRATER")
+// }
+// else if (parsenteg < 80 && parsenteg >= 70) {
+//     console.log("B GRATER")
+// }
+// else if (parsenteg < 70 && parsenteg >= 60) {
+//     console.log("C GRATER")
+// }
+// else if (parsenteg < 60 && parsenteg >= 50) {
+//     console.log("D GRATER")
+// }
+// else if (parsenteg < 50 && parsenteg >= 40) {
+//     console.log("E grater")
+// }
+// else {
+//     console.log("f")
+// }
+
